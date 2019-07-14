1fb9bd269beaaad2b16b08ecf61cf9e2e12f6958
mi4en <marionikolov87@gmail.com>
2019-07-14 12:12:36 +0300

    add components; add moclked todo list; add styles;

---
+++ src/components/Todos.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+
+export default {
+    name: 'Todos',
+    components: {
+        TodoItem
+    },
+    props: {
+        msg: String,
+        todos: Array
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
